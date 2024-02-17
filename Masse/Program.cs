5edb8c157c615ee1883338fab26fefdece8f71c6
Tommaso Castagnoli <tommasocastagnoli29@gmail.com>
2024-02-17 17:27:24 +0100

    Aggiungere i file di progetto.

---
+++ Masse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double m1 = 6.0 * (Math.Pow(10, 24));
+            double m2 = 7.0 * (Math.Pow(10, 22));
+            double m3 = 7.0 * (Math.Pow(10, 22));
+            double m4 = 7.0 * (Math.Pow(10, 22));
+
+            Vector f21 = new Vector();
+            f21.Modulo=(6.607)
+        }
+    }
+}
---
+++ Masse/Vector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masse
+{
+    internal class Vector
+    {
+       
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public double Modulo(Vector v1)
+        {
+            double mod = (Math.Sqrt(X * X + Y * Y));
+            return mod;
+        }
+    }
+}
